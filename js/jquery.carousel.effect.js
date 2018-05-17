859058efbd4af3bc30ee78168a51d476eacf0be7
Sergey Nechaev <slow_cheetah@inbox.ru>
2018-05-17 15:23:27 +0700

    Rename plugin

--- js/jquery.carousel.effect.js
+++ js/jquery.carousel.effect.js
@@ -71,13 +71,13 @@
     },
   };
 
-  $.fn.slideEffect = function (method) {
+  $.fn.carouselEffect = function (method) {
     if (methods[method]) {
       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
     } else if (typeof method === 'object' || !method) {
       return methods.init.apply(this, arguments);
     } else {
-      $.error('Method ' + method + ' not found in jQuery.slideEffect');
+      $.error('Method ' + method + ' not found in jQuery.carouselEffect');
     }
   };
 
